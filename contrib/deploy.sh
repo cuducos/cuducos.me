71d3a108bc1be55d8841706a98339652004aa75a
Eduardo Cuducos <cuducos@gmail.com>
2016-05-18 10:52:35 -0300

    Try to deploy only if envvar is set

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,6 +1,10 @@
+if [ -n "$CUDUCOSME" ]; then
+    echo '\n==> Generating the site at _site/'
+    JEKYLL_ENV=production bundle exec jekyll build
 
-echo '\n==> Generating the site at _site/'
-JEKYLL_ENV=production bundle exec jekyll build
-
-echo '\n==> Syncing with the remote sever'
-rsync -crvz --delete _site/ $CUDUCOSME
+    echo '\n==> Syncing with the remote sever'
+    rsync -crvz --delete _site/ $CUDUCOSME
+else
+    echo '\nPlease, set the environment variable `CUDUCOSME` with the server SSH info.'
+    echo '(e.g. `export CUDUCOSME=user:secret@server.org:/users/user/www/`)\n'
+fi
