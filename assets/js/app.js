fdede854405bcdac1d21a8f32a7f591fb8de47ac
Eduardo Cuducos <cuducos@gmail.com>
2016-05-19 10:18:49 -0300

    Track outbound links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,60 @@
+// Add rot13 as a String method
 String.prototype.rot13 = function () {
   return this.replace(/[a-zA-Z]/g, function (c) {
     return String.fromCharCode(((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26));
   });
 };
 
+// Google Analytics: function to send info about external links
+var trackOutboundLink = function (e) {
+  var anchor = getAnchor(e.target);
+  var url = anchor.href;
+
+  if (typeof ga === 'function') {
+    ga('send', 'event', 'outbound', 'click', url, {
+      transport: 'beacon',
+      hitCallback: function () {
+        document.location = url;
+      },
+    });
+    return false;
+  }
+
+  document.location = url;
+  return true;
+};
+
+// Google Analytics: track all external links
+var attachTrackOutboundLink  = function () {
+  var hostname = new RegExp(location.hostname);
+  var links = document.getElementsByTagName('a');
+  Array.prototype.slice.call(links, 0).map(function (link) {
+    if (!hostname.test(link.hostname)) link.onclick = trackOutboundLink;
+  });
+};
+
+// Get <a /> if event/click is in a child
+var getAnchor = function (element) {
+  if (element.nodeName === 'A') return element;
+  return getAnchor(element.parentElement);
+};
+
+// Decode email link
 var includeEmailLink = function () {
   var anchor = document.getElementById('hello');
   var href = anchor.getAttribute('href');
   return anchor.setAttribute('href', href.rot13());
 };
 
+// Timer to call includeEmailLink
 var includeEmailLinkTimer = function () {
   return setTimeout(includeEmailLink, 7654);
 };
 
-window.addEventListener('DOMContentLoaded', includeEmailLinkTimer, false);
+// Everthing that has to be run when DOM is loaded
+var init = function () {
+  includeEmailLinkTimer();
+  attachTrackOutboundLink();
+};
+
+window.addEventListener('DOMContentLoaded', init, false);
