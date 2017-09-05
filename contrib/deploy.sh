dca0bc53b62f36fceffc450ecfa2cd6dc59d1032
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-09-05 12:22:23 -0300

    Deploy to S3

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,26 +1,14 @@
-if [ -n "$CUDUCOSME" ]; then
+echo '\n==> Cleaning system created files'
+find . -iname '.DS_Store' -delete
 
-    echo '\n==> Cleaning system created files'
-    find . -iname '.DS_Store' -delete
+echo '\n==> Instaling NodeJS packages'
+npm install
 
-    echo '\n==> Cleaning _site/'
-    bundle exec jekyll clean
+echo '\n==> Generating assets'
+npm run assets
 
-    echo '\n==> Instaling NodeJS packages'
-    npm install
+echo '\n==> Generating the site at _site/'
+JEKYLL_ENV=production bundle exec jekyll build
 
-    echo '\n==> Generating assets'
-    npm run assets
-
-    echo '\n==> Generating the site at _site/'
-    JEKYLL_ENV=production bundle exec jekyll build
-
-    echo '\n==> Syncing with the remote sever'
-    rsync -crvz --delete _site/ $CUDUCOSME
-
-else
-
-    echo '\nPlease, set the environment variable `CUDUCOSME` with the server SSH info.'
-    echo '(e.g. `export CUDUCOSME=user:secret@server.org:/users/user/www/`)\n'
-
-fi
+echo '\n==> Syncing with the remote sever'
+aws s3 sync _site s3://cuducos.me --delete
