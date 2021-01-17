7072cc8defc4b717471dc35a234d27e42519377e
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-01-17 15:53:48 -0500

    Fixes search feature

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -15,4 +15,22 @@ window.onclick = function(event) {
         }
     }
 }
+//
+// Add site boundary to Duck Duck Go search box
+//
 
+const submitSearch = function () {
+  fields = document.querySelectorAll(".ddg input[name=q]");
+  Array.prototype.slice.call(fields).map(function (obj) {
+    obj.value = obj.value + " site:cuducos.me";
+  });
+};
+
+//
+// Run, Forrest, run!
+//
+
+var forms = document.querySelectorAll(".ddg");
+Array.prototype.slice.call(forms).map(function (obj) {
+  obj.addEventListener("submit", submitSearch);
+});
